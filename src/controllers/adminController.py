0bfcbb6cd71879093dbc7689cffe770b35c654ee
xBob45 <xmorav45@vutbr.cz>
2023-10-29 21:23:23 +0100

    Admin panel CSS changes.

--- src/controllers/adminController.py
+++ src/controllers/adminController.py
@@ -13,16 +13,17 @@ def add_user():
         first_name = request.form.get("add_fname")
         last_name = request.form.get("add_lname")
         password = request.form.get("add_pass")
-        role_name = request.form.get("gridRadios")
+        role_name = request.form.get("add_role")
         role = Role.query.filter_by(name=role_name).first()
         print(role)
         user = User(username=username, first_name=first_name, last_name=last_name, password=password)
         user.roles.append(role)
         db.session.add(user)
         db.session.commit()
+        db.session.close()
         #-------------------------------------------Reflected XSS - START--------------------------------------------
         flash("User <strong>%s</strong> has sucesfully been added to the database." % (username))
-        #-------------------------------------------Reflected XSS - END--------------------------------------------
+        #-------------------------------------------Reflected XSS - END----------------------------------------------
         #This implements the  Post/Redirect/Get (PRG) to prevent data re-insertion when reload.
         return redirect(url_for('admin.add_user'))
     return render_template("admin/admin_panel_add.html")
@@ -34,16 +35,57 @@ def view_user():
         print(username)
         user = User.query.filter_by(username=username).first()
         print(user)
-        if user:
+        if user is not None:
             return render_template("admin/admin_panel_view_and_update.html", user=user)
-
+        else:
+            flash("User <strong>%s</strong> doesn't exists." % (username))   
     return render_template("admin/admin_panel_view_and_update.html")
+
 
 def update_user():
-    flash("User has been updated.")
-    username = request.form.get("edit_username")
-    print(username)
+    if request.method == 'POST':
+        id = request.form.get("edit_id")
+        user = User.query.filter_by(id=id).first()
+        user.username = request.form.get("edit_username")
+        user.first_name = request.form.get("edit_fn")
+        user.last_name = request.form.get("edit_ln")
+        user.password = request.form.get("edit_password")
+
+        new_role_name = request.form.get("edit_role")
+        
+        new_role = Role.query.filter_by(name=new_role_name).first()
+        
+        if new_role:
+            if new_role not in user.roles:
+                for user_role in user.roles:
+                    user.roles.remove(user_role)
+                user.roles.append(new_role)
+                db.session.commit()
+                db.session.close()
+                flash("User has been updated.")
+            else:
+                flash("Error occured")
+        else:
+            flash("Error occured")
+
     return render_template("admin/admin_panel_view_and_update.html")
 
+
+
 def delete_user():
+    if request.method == 'POST':
+        username = request.form.get("delete_username")
+        print(username)
+        user = User.query.filter_by(username=username).first()
+        if user is not None:
+            db.session.delete(user)
+            db.session.commit()
+            db.session.close()
+            #-------------------------------------------Reflected XSS - START--------------------------------------------
+            flash("User <strong>%s</strong> has been deleted." % (username))
+        else:
+            flash("User <strong>%s</strong> doesn't exists." % (username))
+            #-------------------------------------------Reflected XSS - END----------------------------------------------
+            return redirect(url_for('admin.delete_user'))
+    
     return render_template("admin/admin_panel_delete.html")
